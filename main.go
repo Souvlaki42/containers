a97f8475dec05ed336c10f61d2a43201228a50fe
Souvlaki42 <ilias@moulas.dev>
2026-05-09 02:36:07 +0300

    feat: arg boilerplate

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// docker/podman run <image> <cmd> <...params>
+// go run main.go run <image> <cmd> <...params>
+
+func run() {
+	fmt.Printf("Running %s...\n", strings.Join(os.Args[2:], " "))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s run <image> <cmd> <...params>\n", os.Args[0])
+		os.Exit(0)
+	}
+
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		fmt.Println("Uknown command")
+		os.Exit(2)
+	}
 }
