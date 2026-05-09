92d216ba620f087bcbfcc7bb915e851f8cf77231
Souvlaki42 <ilias@moulas.dev>
2026-05-09 03:18:07 +0300

    feat: new processes

--- main.go
+++ main.go
@@ -3,14 +3,50 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
+	"syscall"
 )
 
 // docker/podman run <image> <cmd> <...params>
 // go run main.go run <image> <cmd> <...params>
 
+func child() {
+	fmt.Printf("Running %s...\n", strings.Join(os.Args[2:], " "))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+	syscall.Sethostname([]byte("container"))
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func run() {
 	fmt.Printf("Running %s...\n", strings.Join(os.Args[2:], " "))
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func main() {
@@ -22,6 +58,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		fmt.Println("Uknown command")
 		os.Exit(2)
